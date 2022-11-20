807cb13b85cafc2079f4801a1396f0209acf2982
Kamil <letsgamble92@gmail.com>
2022-11-20 13:05:30 +0100

    final version

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 def days_between_higher_temp(temperatures):
 	counter = 1
 	list_of_days = []
-	for val in range(0, len(temperatures)):
-		for val2 in range(val + 1, len(temperatures)):
+	for val in range(0, len(temperatures) - 1):
+		for val2 in range(val + 1, len(temperatures) - 1):
 			if temperatures[val] < temperatures[val2]:
 				break
-			elif temperatures[val] >= temperatures[val2]:
+			else:
 				counter += 1
 				if counter == len(temperatures) - val:
+					print(val2)
 					counter = 0
 		list_of_days.append(counter)
 		counter = 1
+	list_of_days.append(0)
 	return list_of_days
 
 
-days_between_higher_temp([15, -1, 15, -1, -1, 10])
+print(days_between_higher_temp([0, -1, -15, -15, -8, 23]))
